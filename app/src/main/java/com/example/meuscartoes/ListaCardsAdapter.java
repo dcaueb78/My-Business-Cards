bc64950086a887c7e52b5a7200b5cf555657a344
dcaueb78 <caue1017@hotmail.com>
2019-12-09 16:24:21 -0300

    Criado item card

--- app/src/main/java/com/example/meuscartoes/ListaCardsAdapter.java
+++ app/src/main/java/com/example/meuscartoes/ListaCardsAdapter.java
@@ -3,6 +3,7 @@ package com.example.meuscartoes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +13,9 @@ public class ListaCardsAdapter extends RecyclerView.Adapter<ListaCardsAdapter.Li
     @NonNull
     @Override
     public ListaCardsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-//        View view = LayoutInflater.from(parent.getContext()).inflate();
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.itemcard, parent, false);
 
-        return null;
+        return new ListaCardsViewHolder(view);
     }
 
     @Override
@@ -29,8 +30,12 @@ public class ListaCardsAdapter extends RecyclerView.Adapter<ListaCardsAdapter.Li
 
     static class ListaCardsViewHolder extends RecyclerView.ViewHolder {
 
+        private TextView textTituloCard;
+
         public ListaCardsViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            textTituloCard = itemView.findViewById(R.id.text_titulo_card);
         }
     }
 }
