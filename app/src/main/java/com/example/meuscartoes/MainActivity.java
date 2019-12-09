bd3bfc4ee14bf3a3f2894236fa3f61736654d2d8
dcaueb78 <caue1017@hotmail.com>
2019-12-09 13:18:36 -0300

    Alteracao design e transicao de telas

--- app/src/main/java/com/example/meuscartoes/MainActivity.java
+++ app/src/main/java/com/example/meuscartoes/MainActivity.java
@@ -2,26 +2,34 @@ package com.example.meuscartoes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnCreate = (Button) findViewById(R.id.btnCreate);
-        btnCreate.setOnClickListener(this);
+        FloatingActionButton btnAdd = findViewById(R.id.btnCreate);
+        btnAdd.setOnClickListener(addButton);
     }
 
-    @Override
-    public void onClick (View v) {
-        Intent it = new Intent(this, cadastro.class);
-        startActivity(it);
-    }
+    private View.OnClickListener addButton = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast toast=Toast.makeText(getApplicationContext(),"Hello batatinha",Toast.LENGTH_SHORT);
+            toast.show();
 
+            Intent intent = new Intent(MainActivity.this, cadastro.class);
+            startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+        }
+    };
 }
--- app/src/main/java/com/example/meuscartoes/cadastro.java
+++ app/src/main/java/com/example/meuscartoes/cadastro.java
@@ -2,13 +2,31 @@ package com.example.meuscartoes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class cadastro extends AppCompatActivity {
 
+    private Button backButton;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        backButton = (Button) findViewById(R.id.back);
+        backButton.setOnClickListener(backFunction);
     }
+
+    private View.OnClickListener backFunction = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            finish();
+        }
+    };
+
 }
